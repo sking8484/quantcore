45defc9426fcd1f41f4431c19a564c1f9f895b05
Stephen King <sking8484@gmail.com>
2018-08-06 21:39:20 -0400

    SETTINGS ALTERED

--- get_data/settings.py
+++ get_data/settings.py
@@ -134,3 +134,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
