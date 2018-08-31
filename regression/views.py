96781655ecc102e955512980e95d142e30dc16ba
Stephen King <sking8484@gmail.com>
2018-08-31 14:43:48 -0700

    ADDED TRY AND EXCEPT BACK IN TO REGRESSION

--- regression/views.py
+++ regression/views.py
@@ -21,82 +21,87 @@ def regression_home(request):
 
 def simple_regression(request):
     if request.method == "POST":
+        try:
+
+            if request.POST['port_regression']:
+                port_regression = request.POST['port_regression']
+                if port_regression == 'run':
+                    data = port_views.get_optimal_portfolio_table()
+                    data['Returns'] = data['Optimal Position Value']/data['Optimal Position Value'][0]
+                    y = data['Returns']
+
+                    X = data_views.get_stock_data('stock_data', 'SPY', pd.to_datetime('12-12-2010'), datetime.now())
+                    X = X['Adj. Close']
+                    X.rename('SP500', inplace = True)
+                else:
+                    return render(request, 'port_optimization/optimization_form.html')
 
 
-        if request.POST['port_regression']:
-            port_regression = request.POST['port_regression']
-            data = port_views.get_optimal_portfolio_table()
-            data['Returns'] = data['Optimal Position Value']/data['Optimal Position Value'][0]
-            y = data['Returns']
+            else:
 
-            X = data_views.get_stock_data('stock_data', 'SPY', pd.to_datetime('12-12-2010'), datetime.now())
-            X = X['Adj. Close']
-            X.rename('SP500', inplace = True)
-        else:
+                """GETTING THE DATA"""
+                datatype_1 = request.POST['datatype1']
+                datatype_2 = request.POST['datatype2']
+                ticker1 = request.POST['symbol1'].upper()
+                ticker2 = request.POST['symbol2'].upper()
+                start = pd.to_datetime(request.POST['start'])
+                end = pd.to_datetime(request.POST['end'])
+                # try:
+                if datatype_1 == 'stock_data':
+                    y = data_views.get_stock_data(datatype_1, ticker1, start, end)
+                    y = y['Adj. Close']
+                    y.rename(ticker1, inplace = True)
 
-            """GETTING THE DATA"""
-            datatype_1 = request.POST['datatype1']
-            datatype_2 = request.POST['datatype2']
-            ticker1 = request.POST['symbol1'].upper()
-            ticker2 = request.POST['symbol2'].upper()
-            start = pd.to_datetime(request.POST['start'])
-            end = pd.to_datetime(request.POST['end'])
-            # try:
-            if datatype_1 == 'stock_data':
-                y = data_views.get_stock_data(datatype_1, ticker1, start, end)
-                y = y['Adj. Close']
-                y.rename(ticker1, inplace = True)
+                if datatype_1=='real_estate':
+                    y = data_views.get_stock_data(datatype_1,ticker1,start,end)
+                    y.columns = ([ticker1])
+                if datatype_1 == 'economic':
+                    y = data_views.get_stock_data(datatype_1, ticker1, start, end)
+                    y.columns = ([ticker1])
 
-            if datatype_1=='real_estate':
-                y = data_views.get_stock_data(datatype_1,ticker1,start,end)
-                y.columns = ([ticker1])
-            if datatype_1 == 'economic':
-                y = data_views.get_stock_data(datatype_1, ticker1, start, end)
-                y.columns = ([ticker1])
+                if datatype_2 == 'stock_data':
+                    X = data_views.get_stock_data(datatype_2, ticker2, start, end)
+                    X = X['Adj. Close']
+                    X.rename(ticker2, inplace = True)
 
-            if datatype_2 == 'stock_data':
-                X = data_views.get_stock_data(datatype_2, ticker2, start, end)
-                X = X['Adj. Close']
-                X.rename(ticker2, inplace = True)
-
-            if datatype_2 == 'real_estate':
-                X = data_views.get_stock_data(datatype_2,ticker2,start,end)
-                X.columns = ([ticker2])
-            if datatype_2 == 'economic':
-                X = data_views.get_stock_data(datatype_2, ticker2, start, end)
-                X.columns = ([ticker2])
+                if datatype_2 == 'real_estate':
+                    X = data_views.get_stock_data(datatype_2,ticker2,start,end)
+                    X.columns = ([ticker2])
+                if datatype_2 == 'economic':
+                    X = data_views.get_stock_data(datatype_2, ticker2, start, end)
+                    X.columns = ([ticker2])
 
 
 
-        """ACTUAL REGRESSION PART"""
+            """ACTUAL REGRESSION PART"""
 
-        dataframe = pd.DataFrame(X)
-        dataframe = dataframe.join(y, how = 'outer')
-        dataframe = dataframe.sort_index(ascending = False)
-        dataframe.dropna(inplace = True)
-        print(dataframe)
+            dataframe = pd.DataFrame(X)
+            dataframe = dataframe.join(y, how = 'outer')
+            dataframe = dataframe.sort_index(ascending = False)
+            dataframe.dropna(inplace = True)
+            print(dataframe)
 
-        X = dataframe.iloc[:,:1]
-        X_name = dataframe.columns[0]
-        y_name = dataframe.columns[1]
-        y = dataframe.iloc[:,-1]
-
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
-        from sklearn.linear_model import LinearRegression
-        regressor = LinearRegression()
-        regressor.fit(X_train, y_train)
-        y_pred = regressor.predict(X_train)
+            X = dataframe.iloc[:,:1]
+            X_name = dataframe.columns[0]
+            y_name = dataframe.columns[1]
+            y = dataframe.iloc[:,-1]
 
 
-        regression_plot = plots.plot_regression(X_test, X_train, y_train, y_test, y_pred, X_name, y_name)
-        X = np.append(arr = np.ones((len(X), 1)).astype(int), values =X , axis = 1)
-        regressor_OLS = sm.OLS(endog=y , exog=X).fit()
-        summary = regressor_OLS.summary().as_html()
-        # except Exception as e:
-        #     error_message = e
-        #     error = 'One or more of your inputs was not accepted: '
-        #     return render(request,'regression/regression.html', {'error':error, 'error_message':error_message})
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+            from sklearn.linear_model import LinearRegression
+            regressor = LinearRegression()
+            regressor.fit(X_train, y_train)
+            y_pred = regressor.predict(X_train)
+
+
+            regression_plot = plots.plot_regression(X_test, X_train, y_train, y_test, y_pred, X_name, y_name)
+            X = np.append(arr = np.ones((len(X), 1)).astype(int), values =X , axis = 1)
+            regressor_OLS = sm.OLS(endog=y , exog=X).fit()
+            summary = regressor_OLS.summary().as_html()
+        except Exception as e:
+            error_message = e
+            error = 'One or more of your inputs was not accepted: '
+            return render(request,'regression/regression.html', {'error':error, 'error_message':error_message})
 
         return render(request, 'regression/simple_regression.html', {'regression_plot':regression_plot,
                                                                     'summary':summary})
